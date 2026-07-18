913d8a009ac4bd2da1a64b17545adeb411e5b1a2
Ericvasconcellos23 <vasconcelloseric23@gmail.com>
2026-07-18 15:59:21 -0300

    operadores, aritmetico, relacioanis e logicos

---
+++ operadores_aritmetico_relacioanis_e_logicos.py
@@ -0,0 +1,39 @@
+# Operadores Aritiméticos
+
+a = 15
+b = 3
+
+print(a + b) # Soma
+print(a - b) # Subtração
+print(a * b) # Multiplicação
+print(a / b) # Divisão (Resultado Decimal)
+print(a // b) # Divisão inteira
+print(a % b) # Resto da Divisão
+
+# Operadores Relacionais True or False (Boolean)
+x = 15
+y = 5
+
+print(x == y) # igual a
+print(x != y) # diferente de
+print(x <  y) # menor que
+print(x >  y) # maior que
+print(x >= y) # maior ou igual a
+print(x <= y) # menor ou igual a
+
+# Operadores Lógicos ( and, or e not)
+#
+# print(True and True)  # ambos iguais é verdadeiro (True)
+# print(True and False) # ambos diferentes é falso (False)
+# print(True or False)  # ambos diferentes é verdadeiro (True)
+# print(True or True)   # ambos iguais é verdadeiro (True)
+# print(not True) # resuldado é falso (False)
+# print(not False) # resultado é verdadeiro (True)
+
+usuario_ativo = True
+senha_correta = False
+
+print(usuario_ativo and senha_correta)
+print(usuario_ativo or senha_correta)
+print(not usuario_ativo)
+
