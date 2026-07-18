3e5e028ef8572060d92e7d245be3af28bd9ce08c
Ericvasconcellos23 <vasconcelloseric23@gmail.com>
2026-07-18 15:31:21 -0300

    Tipos de dados primitivos em Python

---
+++ tipos_de_dados.py
@@ -0,0 +1,48 @@
+# dados do tipo inteiro int
+status_code = 200
+quantidade_usuarios = 15
+tentativas = 5
+
+print(status_code)
+print(quantidade_usuarios)
+print(tentativas)
+
+# dados do tipo flutuante float
+tempo_resposta = 1.42
+valor_total = 199.90
+
+print(tempo_resposta)
+print(valor_total)
+
+# dados do tipo texto str
+nome = 'Luana'
+sobrenome = 'Barbiere'
+
+print(nome)
+print(sobrenome)
+
+# dados do tipo boolean bool
+login_valido = True
+usuario_ativo = False
+
+print(login_valido)
+print(usuario_ativo)
+
+# dados do tipo nulo None
+token = None
+print(token)
+
+# Descobrindo o tipo da variável
+
+tempo = 1.5
+print(type(tempo))
+mensagem = "OK"
+print(type(mensagem))
+
+
+
+
+
+
+
+
