7ddf2f2855bffcd3fee205d172f5f351c9ca4db7
Ericvasconcellos23 <vasconcelloseric23@gmail.com>
2026-07-18 16:13:11 -0300

    entrada e saida de dados

---
+++ entrada_e_saida_de_dados.py
@@ -0,0 +1,18 @@
+# Saída de dados usamos o Print
+print('meu primeiro nome')
+print(10)
+print(3.15)
+print(True)
+print(False)
+
+# Entrada e Saída de dados.
+# Para a entrada usamos o input
+nome = input('Digite o seu Nome:')
+idade = input('Digite sua idade:')
+altura = input('Digite sua altura:')
+print(nome)
+print(idade)
+print(altura)
+print(type(nome))
+print(type(idade))
+print(type(altura))
