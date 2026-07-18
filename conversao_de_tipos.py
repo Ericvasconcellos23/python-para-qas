3e509b54f874be42e74c775445aa484547a094a8
Ericvasconcellos23 <vasconcelloseric23@gmail.com>
2026-07-18 16:34:24 -0300

    conversão de tipos de dados

---
+++ conversao_de_tipos.py
@@ -0,0 +1,17 @@
+# # Conversão de tipos (Casting)
+# valor1 = input('Digite um número: ')
+# valor2 = input('Digite outro número: ')
+#
+# valor1 = int(valor1)
+# valor2 = int(valor2)
+#
+# print(valor1 + valor2)
+
+# Exemplo usando os tipos
+nome = str(input('Digite seu nome: '))
+idade = int(input('Digite sua idade: '))
+altura = float(input('Digite sua altura: '))
+
+print(nome)
+print(idade)
+print(altura)
